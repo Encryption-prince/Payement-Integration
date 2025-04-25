6885a2f68975837e9f2450e9071c9e789c2a2d67
Encryption-prince <subhammaity6827@gmail.com>
2025-04-25 22:31:30 +0530

    added home controller

---
+++ src/main/java/com/spring/implementation/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.spring.implementation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HomeController {
+
+    @GetMapping("/homegreet")
+    public String greet(){
+        return "Home Greet";
+    }
+}
